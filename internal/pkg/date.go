540ff446c5041c97060fa0ba5ea2d21f61905083
Aimer Neige <aimer.neige.soft@gmail.com>
2022-08-09 16:50:49 +0800

    remove useless

--- internal/pkg/date.go
+++
@@ -1,9 +0,0 @@
-package pkg
-
-import "time"
-
-// GetTodayDate 获得今天的日期字符串 20220801
-func GetTodayDate() string {
-	loc, _ := time.LoadLocation("Asia/Shanghai")
-	return time.Now().In(loc).Format("20060102")
-}
